85eb0342d274d2dfbf9cd4a73dccd2ad63ce7264
layanmeta <66564396+layanmeta@users.noreply.github.com>
2021-03-11 01:34:38 +0200

    Add files via upload

---
+++ ThreadExam31/ThreadExam31/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ThreadExam31
+{
+    class Program
+    {
+        public void devideByZero()
+        {
+            try
+            {
+                var results = Task.FromResult(DivideBy0());
+               
+            }
+            catch
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+        }
+
+        private int DivideBy0()
+        {
+            var number = 2;
+            return number / 0;
+        }
+        
+        //a Yes
+        //b still crashes
+        //D = System.Threading.Tasks.UnobservedTaskExceptionEventArgs
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
---
+++ ThreadExam33/ThreadExam33/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadExam33
+{
+    class Program
+    {
+        static CancellationTokenSource cts = new CancellationTokenSource();
+        static int count = 0;
+       static public void MyTimer()
+        {
+           
+            for (int i = 0; i == count; i++)
+            {
+                count++;
+                Thread.Sleep(1000);
+                Console.WriteLine("Increased the timer. Count: " + count);
+            }
+            
+           
+        }
+        static void Main(string[] args)
+        {
+            Task t = new Task(() =>
+            {
+                MyTimer();
+            },cts.Token);
+            Console.WriteLine("Timer is starting");
+            t.Start();
+            Console.ReadLine();
+            cts.Cancel();
+            Console.WriteLine($"timer stopped {count}" );
+
+        }
+    }
+}
